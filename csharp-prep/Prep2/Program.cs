be04706c3db08a2c9f222c779fe62bb97d90bfa5
Ryan Earwood <ear24003@byui.edu>
2024-04-26 17:26:15 -0400

    Finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+        string sign = ""; 
+
+        
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        
+        int lastDigit = percent % 10;
+        if (letter != "A" && letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+       
+        if (percent >= 97 && letter == "A")
+        {
+            sign = ""; 
+        }
+        else if (percent < 60)
+        {
+            sign = ""; 
+        }
+
+        
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You have passed.");
+        }
+        else
+        {
+            Console.WriteLine("You have failed.");
+        }
     }
 }
