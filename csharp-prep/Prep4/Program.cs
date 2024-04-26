8437d480b1d3ca0c0e2537eba0d2af44dc10ac40
Ryan Earwood <ear24003@byui.edu>
2024-04-26 17:39:40 -0400

    Finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -2,8 +2,39 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 0)
+            {
+                break;
+            }
+
+            numbers.Add(number);
+        }
+
+       
+        int sum = numbers.Sum();
+        double average = Math.Round(numbers.Average(), 3);
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
+       
+        int smallestPositive = numbers.Where(n => n > 0).DefaultIfEmpty(0).Min();
+        List<int> sortedNumbers = numbers.OrderBy(n => n).ToList();
+
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        Console.WriteLine("The sorted list is:");
+        sortedNumbers.ForEach(n => Console.WriteLine(n));
     }
 }
