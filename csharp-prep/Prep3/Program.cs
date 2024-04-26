9e845d6a39780c43e5237ea95a71d8eb1cd2c8c9
Ryan Earwood <ear24003@byui.edu>
2024-04-26 17:32:29 -0400

    Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,43 @@
 using System;
 
-class Program
+class MagicNumberGame
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber, guess, numberOfGuesses;
+        string playAgain;
+
+        do
+        {
+            magicNumber = random.Next(1, 101); 
+            numberOfGuesses = 0;
+            guess = 0;
+
+            Console.WriteLine("A magic number between 1 and 100 has been chosen.");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                numberOfGuesses++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+
+            Console.WriteLine($"You guessed it in {numberOfGuesses} guesses!");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thank you for playing!");
     }
 }
